a1d56ecd3708015a76f7608829a371c6df0bbd0f
Gabor Recski <gabor.recski@tuwien.ac.at>
2024-06-05 09:22:35 +0200

    bugfix and refactoring in ud_graph.subgraph

--- tuw_nlp/graph/ud_graph.py
+++ tuw_nlp/graph/ud_graph.py
@@ -1,5 +1,3 @@
-import logging
-
 import networkx as nx
 from networkx.readwrite import json_graph
 from stanza.models.common.doc import Sentence
@@ -17,7 +15,9 @@ class UDGraph(Graph):
         G = json_graph.adjacency_graph(data["graph"])
         assert G.graph["type"] == "ud"
         assert G.graph["text"] == text
-        assert G.graph["tokens"] == tokens
+        assert (
+            G.graph["tokens"] == tokens
+        ), f'tokens mismatch:\n{G.graph["tokens"]=}\n{tokens=}'
         ud_graph = UDGraph(sen, text, tokens)
         ud_graph.G = G
         return ud_graph
@@ -73,67 +73,75 @@ class UDGraph(Graph):
         self.text = None
         self.G.remove_nodes_from(g_to_remove)
 
-    def subgraph(self, nodes, handle_unconnected=None):
-        # print("main graph:", self.str_nodes(), "nodes:", nodes)
+    def _subgraph_infer_new_nodes(self, nodes, handle_unconnected):
         H = self.G.subgraph(nodes)
-        if not nx.is_weakly_connected(H):
-            if handle_unconnected is None:
-                print(self.to_dot())
-                print(Graph.nx_graph_to_dot(H))
-                raise UnconnectedGraphError(
-                    f"subgraph induced by nodes {nodes} in {self} is not connected and handle_unconnected is not specified"
-                )
-            elif handle_unconnected.startswith("shortest_path"):
-                inferred_nodes = set()
-                new_nodes = set(
-                    nodes
-                )  # a copy of the original nodes parameter to expand
-                components = [
-                    list(node_set) for node_set in nx.weakly_connected_components(H)
-                ]
-                src = components[0][0]  # a dedicated node in a dedicated component
-                G_u = (
-                    self.G.to_undirected()
-                )  # an undirected version of G to search for shortest paths
-                for comp in components[1:]:
-                    path = nx.shortest_path(G_u, src, comp[0])
-                    # print(f'shortest path between {src} and {comp[0]}: {path}')
-                    for node in path:
-                        if node not in new_nodes:
-                            new_nodes.add(node)
-                            if handle_unconnected.endswith("infer"):
-                                logging.debug(f"inferring {node=}")
-                                inferred_nodes.add(node)
 
-                H = self.G.subgraph(new_nodes)
-                for node in inferred_nodes:
-                    H.nodes()[node]["inferred"] = True
-                # return self.subgraph(new_nodes)
+        # case 1: induced subgraph is connected, nothing to do
+        if nx.is_weakly_connected(H):
+            return set(nodes), set()
 
-            else:
-                raise ValueError(
-                    f"unknown value of handle_unconnected: {handle_unconnected}"
-                )
+        # case 2: induced subgraph was expected to be connected, raise error
+        if handle_unconnected is None:
+            print(self.to_dot())
+            print(Graph.nx_graph_to_dot(H))
+            raise UnconnectedGraphError(
+                f"subgraph induced by nodes {nodes} in {self} is not connected and handle_unconnected is not specified"
+            )
+
+        # case 3: induce connected subgraph using shortest paths between components
+        if handle_unconnected == "shortest_path":
+            inferred_nodes = set()
+            new_nodes = set(nodes)  # a copy of the original nodes parameter to expand
+            components = [
+                list(node_set) for node_set in nx.weakly_connected_components(H)
+            ]
+            src = components[0][0]  # a dedicated node in a dedicated component
+            G_u = (
+                self.G.to_undirected()
+            )  # an undirected version of G to search for shortest paths
+            for comp in components[1:]:
+                path = nx.shortest_path(G_u, src, comp[0])
+                # print(f'shortest path between {src} and {comp[0]}: {path}')
+                for node in path:
+                    if node not in new_nodes:
+                        new_nodes.add(node)
+                        inferred_nodes.add(node)
+
+            return new_nodes, inferred_nodes
+
+        else:
+            raise ValueError(
+                f"unknown value of handle_unconnected: {handle_unconnected}"
+            )
+
+    def subgraph(self, nodes, handle_unconnected=None):
+        new_nodes, inferred_nodes = self._subgraph_infer_new_nodes(
+            nodes, handle_unconnected
+        )
+
+        # snippet from networkx docs on networkx.Graph.subgraph
+        H = nx.DiGraph()
+        H.add_nodes_from((n, self.G.nodes[n]) for n in new_nodes)
+        H.add_edges_from(
+            (n, nbr, d)
+            for n, nbrs in self.G.adj.items()
+            if n in new_nodes
+            for nbr, d in nbrs.items()
+            if nbr in new_nodes
+        )
+
+        for node in inferred_nodes:
+            H.nodes()[node]["inferred"] = True
 
         tok_ids_to_keep = {data.get("token_id") for node, data in H.nodes(data=True)}
         new_tokens = [
             tok if i + 1 in tok_ids_to_keep else None
             for i, tok in enumerate(self.tokens)
         ]
-        # print("main tokens:", self.tokens)
-        # print("H.nodes:", H.nodes(data=True))
-        # print("new tokens:", new_tokens)
-
         H.graph["tokens"] = new_tokens
+
         new_graph = UDGraph(self.stanza_sen, text=None, tokens=new_tokens)
-        # print("new graph tokens:", new_graph.tokens)
         new_graph.G = H
-        # print(
-        #    "new graph (subgraph):",
-        #    new_graph.str_nodes(),
-        #    "nodes:",
-        #    new_graph.G.nodes(),
-        # )
         return new_graph
 
     def pos_edge_graph(self, vocab):
@@ -160,7 +168,7 @@ class UDGraph(Graph):
             if isinstance(word["id"], (list, tuple)):
                 # token representing an mwe, e.g. "vom" ~ "von dem"
                 continue
-            name = word.get("lemma", word['text'])
+            name = word.get("lemma", word["text"])
             G.add_node(i, name=name, token_id=word["id"], upos=word["upos"])
             self.tok_ids_to_nodes[word["id"]] = i
             if word["deprel"] == "root":
